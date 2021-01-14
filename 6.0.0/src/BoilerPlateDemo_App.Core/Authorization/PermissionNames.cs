9f1eee87c50ab390f8278b3fffb3a6fb7a21b9a9
niriksha <niriksha@promactinfo.com>
2021-01-14 15:20:18 +0530

    Add child permission implementation in R & P

--- 6.0.0/src/BoilerPlateDemo_App.Core/Authorization/PermissionNames.cs
+++ 6.0.0/src/BoilerPlateDemo_App.Core/Authorization/PermissionNames.cs
@@ -8,5 +8,11 @@
         public const string Pages_Users_Activation = "Pages.Users.Activation";
 
         public const string Pages_Roles = "Pages.Roles";
+
+        public const string Pages_Applications = "Pages.Applications";
+        public const string Pages_Applications_Create = "Pages.Applications.Create";
+        public const string Pages_Applications_Edit = "Pages.Applications.Edit";
+        public const string Pages_Applications_Delete = "Pages.Applications.Delete";
+        public const string Pages_Applications_View = "Pages.Applications.View";
     }
 }
